38bcbcb5b95d8141bc75ff59b561a8db859c47d4
dfvfdvdff <sdfdfdfds@gmail.com>
2024-11-18 15:51:17 +0700

    add remider

--- remider.dart
+++ remider.dart
@@ -1,3 +1,10 @@
 void main() {
+  int dividend = 10; 
+  int divisor = 3;   
 
+  int quotient = dividend ~/ divisor;
+  int remainder = dividend % divisor;
+
+  print("Quotient: $quotient");
+  print("Remainder: $remainder");
 }
